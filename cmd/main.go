59a13a183569a34ef4395ca6d3d101f58968dc38
apanasyuk.kirill <apanasyuk.kirill@wb.ru>
2024-10-01 20:48:51 +0300

    uncorrect path was fixed

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"log"
-	metadata2 "movie.com/metadata/internal/controller/metadata"
-	httpHandler "movie.com/metadata/internal/handler"
-	"movie.com/metadata/internal/repository/memory"
+	controller "movie.com/internal/controller/metadata"
+	httpHandler "movie.com/internal/handler"
+	"movie.com/internal/repository/memory"
 	"net/http"
 )
 
 func main() {
 	log.Println("Starting the microservices_in_go metadata service")
 	repo := memory.New()
-	ctrl := metadata2.NewController(repo)
+	ctrl := controller.NewController(repo)
 	h := httpHandler.New(ctrl)
 	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
